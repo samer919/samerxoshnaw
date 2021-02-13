c619b98e1ee127e84eccb437571a66c74556a0d4
samer919 <79027639+samer919@users.noreply.github.com>
2021-02-14 02:33:36 +0300

    Update samer.py

--- samer.py
+++ samer.py
@@ -5,66 +5,11 @@
 # [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/
 # Embedded file name: dg
 import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
-os.system('rm -rf .txt')
-for n in range(9999):
-    nmbr = random.randint(1111111, 9999999)
-    sys.stdout = open('.txt', 'a')
-    print nmbr
-    sys.stdout.flush()
-
-try:
-    import requests
-except ImportError:
-    os.system('No Module Named Requests! type:pip2 install requests')
-
-try:
-    import mechanize
-except ImportError:
-    os.system('No Module Named Mechanize! type:pip2 install mechanize')
-    time.sleep(1)
-    os.system('Then type: python2 All.py')
-
-import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, requests, mechanize
-from multiprocessing.pool import ThreadPool
-from requests.exceptions import ConnectionError
-from mechanize import Browser
-reload(sys)
-sys.setdefaultencoding('utf8')
-br = mechanize.Browser()
-br.set_handle_robots(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
-br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
-
-def keluar():
-    print 'Thanks.'
-    os.sys.exit()
-
-
-def acak(b):
-    w = 'ahtdzjc'
-    d = ''
-    for i in x:
-        d += '!' + w[random.randint(0, len(w) - 1)] + i
-
-    return cetak(d)
-
-
-def cetak(b):
-    w = 'ahtdzjc'
-    for i in w:
-        j = w.index(i)
-        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
-
-    x += '\x1b[0m'
-    x = x.replace('!0', '\x1b[0m')
-    sys.stdout.write(x + '\n')
-
-
-def jalan(z):
-    for e in z + '\n':
-        sys.stdout.write(e)
-        sys.stdout.flush()
+os.wjejejdjdjdidjdzjxmxnxnxnxmxmxkxkxkkxk
+djxjxjxjxjxjx
+dkdkdd1000000000000000000000
+pppppppppppppp
+pppppppp()
         time.sleep(0.1)
 
 
@@ -84,25 +29,27 @@ def psb(z):
 
 
 back = 0
-oks = []
+oks = []ppppppepfkckckckc
+
+xmxmxmxmxmdmxmxmxmxmxmmxxmmxxm
 id = []
 cpb = []
 vulnot = '\x1b[31mNot Vuln'
-vuln = '\x1b[32mVuln'
+vuln = '\x1bxmxmxxmxnxmxmx[32mVuln'
 os.system("clear")
 ##### LOGO #####
 logo='''
   _______  _______  _______  _______  _______ 
 (  ____ \(  ___  )(       )(  ____ \(  ____ )
 | (    \/| (   ) || () () || (    \/| (    )|
-| (_____ | (___) || || || || (__    | (____)|
+| (_____ | (__djjdjdjdjdjdj_) || || || || (__    | (____)|
 (_____  )|  ___  || |(_)| ||  __)   |     __)
       ) || (   ) || |   | || (      | (\ (   
 /\____) || )   ( || )   ( || (____/\| ) \ \__
 \_______)|/     \||/     \|(_______/|/   \__/
                                              
 --------------------------------------------------
-
+djdjdjdjddjjd
  Auther   : SAMER XOSHNAW
  Auther   : =========
  Auther  : ================
